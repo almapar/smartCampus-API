503b2982626d1ba7a4bd5b07803728772041ec36
almapar <alexutzamaria68@gmail.com>
2026-04-23 01:24:27 +0100

    Cleaned up the files by deleting the unnecessary test cases

--- src/test/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module uk.ac.westminster.cs.module.test {
-    requires jakarta.ws.rs;
-
-    requires org.junit.jupiter.api;
-
-    requires com.example.archetype.grizzly.module;
-
-    exports uk.ac.westminster.cs.test;
-}
